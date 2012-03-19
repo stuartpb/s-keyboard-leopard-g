1d733ccb664122719375a9a47f2b373d39f35371
Stuart P. Bentley <stuart@testtrack4.com>
2012-03-19 13:50:46 -0700

    Version 2.0- now with real-time leopards

--- leopard.user.js
+++ leopard.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name s/keyboard/leopard/g
-// @version 2.0b
+// @version 2.0
 // @description Replaces the word "keyboard" with "leopard".
 // @match *://*/*
 // @updateURL http://userscripts.org/scripts/source/128626.meta.js
 // @downloadURL https://userscripts.org/scripts/source/128626.user.js
 // ==/UserScript==
+
 
 function leopardize(str) {
   return str.replace(/keyboard/g,"leopard")
@@ -16,42 +17,43 @@ function leopardize(str) {
 }
 
 function replaceTextContent(node) {
+  node.textContent = leopardize(node.textContent)
+}
+
+function changeTextNodes(node) {
   var length, childNodes
   //If this is a text node, leopardize it
   if (node.nodeType == Node.TEXT_NODE) {
-    node.textContent = leopardize(node.textContent)
+    replaceTextContent(node)
   //If this is anything other than a text node, recurse any children
   } else {
     childNodes = node.childNodes
     length = childNodes.length
     for(var i=0; i<length; ++i){
-      replaceTextContent(childNodes[i])
+      changeTextNodes(childNodes[i])
     }
   }
 }
 
-var scheduled_timeout, replacement_running
+var replacement_running
 
-function replaceBodyText() {
-  //set the flag so we won't spawn replacements ad infinitum
-  replacement_running = true
-  //Run the replacement
-  replaceTextContent(document.body)
-  //Unset the flag so future changes will trigger another replacement
-  replacement_running = false
-  //Clear the scheduled timeout, since it just ran
-  scheduled_timeout = null;
-}
-
-function listener(event) {
-  if(!replacement_running) {
-    //Cancel any currently pending timeout
-    if(scheduled_timeout) clearTimeout (scheduled_timeout)
-    //Start a new one
-    scheduled_timeout = setTimeout (replaceBodyText, 50)
+function lock_safe(f,param) {
+  if(!replacement_running){
+    replacement_running = true
+    f(param)
+    replacement_running = false
   }
 }
 
-replaceBodyText()
-document.body.addEventListener ("DOMSubtreeModified", listener, false)
+function insertion_listener(event) {
+  lock_safe(changeTextNodes,event.target)
+}
+
+function cdm_listener(event) {
+  lock_safe(replaceTextContent,event.target)
+}
+
+changeTextNodes(document.body)
+document.body.addEventListener ("DOMNodeInserted", insertion_listener, false)
+document.body.addEventListener ("DOMCharacterDataModified", cdm_listener, false)
 document.title = leopardize(document.title)
