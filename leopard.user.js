bba6ca9383433cd254e8e43e8850d82c9dd8453c
Stuart P. Bentley <stuart@testtrack4.com>
2012-03-21 12:19:50 -0700

    Add license

--- leopard.user.js
+++ leopard.user.js
@@ -1,11 +1,25 @@
 // ==UserScript==
 // @name s/keyboard/leopard/g
-// @version 2.2-beta2
+// @version 2.2
 // @description Replaces the word "keyboard" with "leopard".
 // @match *://*/*
 // @updateURL http://userscripts.org/scripts/source/128626.meta.js
 // @downloadURL https://userscripts.org/scripts/source/128626.user.js
 // ==/UserScript==
+
+// Copyright (c) 2012, Stuart P. Bentley <stuart@testtrack4.com>
+
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 function leopardize(str) {
   return str.replace(/KEYBOARD/g,"LEOPARD")
