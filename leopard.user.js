1be2fe2a7c296d5f7155a3d8d2ad30675aa7e12b
Stuart P. Bentley <stuart@testtrack4.com>
2012-07-23 15:06:58 -0700

    Skip modifying nodes that won't change

--- leopard.user.js
+++ leopard.user.js
@@ -8,29 +8,44 @@
 // @downloadURL https://userscripts.org/scripts/source/128626.user.js
 // ==/UserScript==
 
-//which letters in "keyboard" get replaced with which in "leopard"
+// The pattern to match
+var keyboard_pattern = /(k)(e)(y)(b)o(ard)/ig
+
+// which letters in "keyboard" get replaced with which in "leopard"
 var leopard_subs = {
   'k': 'l', 'K': 'L',
   'y': 'o', 'Y': 'O',
   'b': 'p', 'B': 'P'
 };
 
-//Transform all instances of 'keyboard' in a string into 'leopard'
-function leopardize(str) {
-  return str.replace(/(k)(e)(y)(b)o(ard)/ig, function(match,k,e,y,b,ard){
+// construct "leopard" replacement for "keyboard" components
+function replacement_leopard(match,k,e,y,b,ard){
     return leopard_subs[k] + e +  leopard_subs[y] + leopard_subs[b] + ard
-  })
 }
 
+// Transform all instances of 'keyboard' in a string into 'leopard'
+function leopardize(str) {
+  return str.replace(keyboard_pattern, replacement_leopard)
+}
+
+// define the noteType value for TEXT_NODEs (the kind we want to replace)
+// Some browsers may fail to make this value available - it's 3
 var TEXT_NODE = Node.TEXT_NODE || 3
+
+// Flag to signal that we're replacing text, so that change doesn't trigger
+// another replacement (technically, that can't happen if all the instances
+// of "keyboard" that would trigger a replacement have been replaced with
+// "leopard", but it's still good practice)
 var replacingContent = false
 
 function replaceTextContent(node) {
-  //flag that content is being replaced so the event it generates
-  //won't trigger another replacement
-  replacingContent = true
-  node.textContent = leopardize(node.textContent)
-  replacingContent = false
+  if (~node.textContent.search(keyboard_pattern)) {
+    //flag that content is being replaced so the event it generates
+    //won't trigger another replacement
+    replacingContent = true
+    node.textContent = leopardize(node.textContent)
+    replacingContent = false
+  }
 }
 
 function changeTextNodes(node) {
